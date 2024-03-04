9e76010532df31c69fea6913589d5d4622f18dea
Administrator <admin@example.com>
2024-03-04 10:25:01 +0800

    增加ReturnData返回方法

--- goadmin-gin/ReturnData/ReturnData.go
+++ goadmin-gin/ReturnData/ReturnData.go
@@ -3,6 +3,12 @@ package ReturnData
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
+	"net/http"
+)
+
+const (
+	ERROR   = 400
+	SUCCESS = 200
 )
 
 // 定义返回数据结构体
@@ -13,18 +19,15 @@ type ReturnData struct {
 }
 
 // 构造函数,配置默认值
-func NewReturnData() ReturnData {
+func NewReturnData(status int, message string, data map[string]interface{}) ReturnData {
 	returnData := ReturnData{}
 	returnData.Status = 200
-	data := make(map[string]interface{})
+	returnData.Message = message
 	returnData.Data = data
 	return returnData
 }
 
-type Info struct {
-	ReturnData
-}
-
+// 获取请求入口的数据
 func GetRequestInfo(r *gin.Context, info interface{}) (err error) {
 	// 首先获取请求的类型
 	requestMethod := r.Request.Method
@@ -37,3 +40,52 @@ func GetRequestInfo(r *gin.Context, info interface{}) (err error) {
 	}
 	return err
 }
+
+func Result(status int, message string, data map[string]interface{}, c *gin.Context) {
+	returnData := NewReturnData(status, message, data)
+	c.JSON(http.StatusOK, returnData)
+}
+
+// 操作成功,并返回数据和成功信息
+func OK(message string, data map[string]interface{}, c *gin.Context) {
+	Result(SUCCESS, message, data, c)
+}
+
+// 操作成功,只返回成功信息
+func OKWithMessage(message string, c *gin.Context) {
+	Result(SUCCESS, message, nil, c)
+}
+
+// 操作成功,只返回成功数据
+func OKWithData(data map[string]interface{}, c *gin.Context) {
+	Result(SUCCESS, "操作成功", data, c)
+}
+
+//func OKWithList[T any](List []T, count any, c *gin.Context) {
+//	if len(List) == 0 {
+//		List = []T{}
+//	}
+//	Result(SUCCESS, ListResponse[T]{
+//		List:  List,
+//		Count: count,
+//	}, "成功", c)
+//}
+
+// 操作失败,并返回数据和失败信息
+func Fail(data map[string]interface{}, message string, c *gin.Context) {
+	Result(ERROR, message, data, c)
+}
+
+// 操作失败,并返回失败信息
+func FailWithMessage(message string, c *gin.Context) {
+	Result(ERROR, message, nil, c)
+}
+
+//func FailWithCode(code ErrorCode, c *gin.Context) {
+//	msg, ok := ErrorMap[code]
+//	if ok {
+//		Result(int(code), map[string]interface{}{}, msg, c)
+//		return
+//	}
+//	Result(ERROR, map[string]interface{}{}, "未知错误", c)
+//}
