55619efa0676b498c1c3239c9d3ab728d71f625f
Administrator <admin@example.com>
2024-03-20 18:38:15 +0800

    db

--- goadmin-config/db.go
+++ goadmin-config/db.go
@@ -7,9 +7,9 @@ type DBItemConf struct {
 	Username     string `yaml:"username"`
 	Password     string `yaml:"password"`
 	Config       string `yaml:"config"`
-	Timeout      uint   `yaml:"timeout"`        // connect db timeout , uint ms
-	WriteTimeOut uint   `yaml:"write_time_out"` // write data timeout , uint ms
-	ReadTimeOut  uint   `yaml:"read_time_out"`  // read data timeout,uint ms
+	Timeout      int    `yaml:"timeout"`        // connect db timeout , uint ms
+	WriteTimeOut int    `yaml:"write_time_out"` // write data timeout , uint ms
+	ReadTimeOut  int    `yaml:"read_time_out"`  // read data timeout,uint ms
 	MaxIdleConns int    `yaml:"max_idle_conns"` // 最大的闲置连接数
 	MaxOpenConns int    `yaml:"max_open_conns"` //最大打开连接数
 }
