a2dd8c35aa7c2ecbd3ca8abbfcac9fb91a78ec4b
Administrator <admin@example.com>
2024-03-21 09:06:16 +0800

    config

--- goadmin-config/db.go
+++ goadmin-config/db.go
@@ -7,11 +7,11 @@ type DBItemConf struct {
 	Username     string `yaml:"username"`
 	Password     string `yaml:"password"`
 	Config       string `yaml:"config"`
-	Timeout      int    `yaml:"timeout"`        // connect db timeout , uint ms
-	WriteTimeOut int    `yaml:"write_time_out"` // write data timeout , uint ms
-	ReadTimeOut  int    `yaml:"read_time_out"`  // read data timeout,uint ms
-	MaxIdleConns int    `yaml:"max_idle_conns"` // 最大的闲置连接数
-	MaxOpenConns int    `yaml:"max_open_conns"` //最大打开连接数
+	Timeout      int    `yaml:"timeout"`                                      // connect db timeout , uint ms
+	WriteTimeOut int    `yaml:"write_time_out" mapstructure:"write_time_out"` // write data timeout , uint ms
+	ReadTimeOut  int    `yaml:"read_time_out" mapstructure:"read_time_out"`   // read data timeout,uint ms
+	MaxIdleConns int    `yaml:"max_idle_conns" mapstructure:"max_idle_conns"` // 最大的闲置连接数
+	MaxOpenConns int    `yaml:"max_open_conns" mapstructure:"max_open_conns"` //最大打开连接数
 }
 
 type DBItem struct {
