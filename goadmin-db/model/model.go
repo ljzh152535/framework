d2876afc5058f84c92d2b306075fcdd840a5fd48
Administrator <admin@example.com>
2024-04-30 14:23:52 +0800

    model

--- goadmin-db/model/model.go
+++ goadmin-db/model/model.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
 type DBItemConf struct {
 	Host         string `yaml:"host"`
 	Port         string `yaml:"port"`
@@ -26,4 +31,11 @@ type DBItem struct {
 	Write DBItemConf `yaml:"write"`
 	Read  DBItemConf `yaml:"read"`
 	Log   DBLog      `yaml:"log"`
+}
+
+type GVA_MODEL struct {
+	ID        uint           `json:"id" gorm:"primarykey" form:"id"` // 主键ID
+	CreatedAt time.Time      // 创建时间
+	UpdatedAt time.Time      // 更新时间
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 删除时间
 }
